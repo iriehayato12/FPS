77e0e5ff2f7a37cb674e6eaca9b430aa481da065
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-11-02 09:07:13 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  #管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+      sessions: "admin/sessions"
+  }
+  
+  #ユーザー用
+  devise_for :users, skip: [:password], controllers: {
+      registrations: "public/registrations",
+      sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+   get "/" => "homes#top"
+  end
+  
+  scope module: :public do
+   root "homes#top"
+   get "about" => "homes#about"
+  end
+  
+  
 end
