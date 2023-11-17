c9f6a6025236b46b32148349c52b23d732e1ae5e
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-11-17 02:56:10 +0000

    修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,5 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :favorites]
   before_action :ensure_guest_user, only: [:edit]
 
   def show
