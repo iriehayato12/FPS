8d6ba23bb6867b96108186a364cadca4509ce51d
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-11-28 04:04:50 +0000

    テストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-Admin.create!(
-   email: 'admin@admin',
-   password: 'admin1'
-)
+
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
@@ -11,3 +8,75 @@ Admin.create!(
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.find_or_create_by!(email: "admin@admin") do |admin| 
+   admin.password = 'admin1'
+end
+
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.introduction = "ID：1234567 ３年ぐらいFPSしてます！！"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.introduction = "FPSは初めてなので優しくしてください～"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.introduction = "FPS歴10年です！！ランク主体でプレーしてます。"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
+
+Report.find_or_create_by!(reason:"不適切な表現") do |report|
+   report.reporter_id = "1"
+   report.reported_id = "3"
+   report.status = 0
+end
+
+Category.create(name: "APEX", id: 1)
+Category.create(name: "VALORANT", id: 2) 
+Category.create(name: "overwatch", id: 3)
+Category.create(name: "COD", id: 4)
+
+
+Post.find_or_create_by!(title: "フレンド募集！！") do |post|
+  post.body = "初心者大歓迎です！フレンドになりましょう"
+  post.user = olivia
+  post.user_id = "1"
+  post.category_id = "1"
+end
+
+Post.find_or_create_by!(title: "ランクメンバー募集！！") do |post|
+  post.body = "ランク帯がゴールド以上の方募集中！！"
+  post.user = lucas
+  post.user_id = "3"
+  post.category_id = "2"
+end
+
+Post.find_or_create_by!(title: "まったりしましょう") do |post|
+  post.body = '基本ランク以外でプレーしましょう！緩い雰囲気で～～'
+  post.category_id = "3"
+  post.user_id = "2"
+  post.user = james
+end
+  
+PostComment.find_or_create_by!(comment: "フレンドになりましょう！！") do |comment|
+   comment.user = james
+   comment.post_id = "1"
+end
+
+PostComment.find_or_create_by!(comment: "ぜひお願いします") do |comment|
+   comment.user = olivia
+   comment.post_id = "1"
+end
+
+
+
